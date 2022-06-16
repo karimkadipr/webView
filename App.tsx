ef0d3b5073066f19dae018711332c98db3294614
KadiKarimPr <kadikarimpr@smartinia.com>
2022-06-16 13:06:24 +0200

    Adding input for chosing webview

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {
   Image,
   SafeAreaView,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 import WebView from 'react-native-webview';
@@ -16,64 +17,89 @@ const windowHeight = Dimensions.get('window').height;
 const App = () => {
   const [accessWebView, setAccessWebView] = useState(false);
   const [seeDialog, setSeeDialog] = useState(false);
-  const [timer, setTimer] = useState(10);
+  //const [timer, setTimer] = useState(3);
+  const [url, setUrl] = useState('https://www.youtube.com/');
 
-  useEffect(() => {
+  /*   useEffect(() => {
     setTimeout(() => {
       setAccessWebView(true);
-    }, 10000);
+    }, 3000);
 
     setTimeout(() => {
       setSeeDialog(true);
-    }, 11000);
+    }, 4000);
 
     setInterval(() => {
       if (timer >= 0) {
         setTimer(prev => prev - 1);
       }
     }, 1000);
-  }, []);
+  }, []); */
 
   return (
     <View style={{backgroundColor: 'white'}}>
-      {!accessWebView && (
-        <>
-          <Image
-            source={{
-              uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-            }}
-            style={{
-              width: windowWidth,
-              height: windowHeight,
-              opacity: 0.6,
-              zIndex: -99,
-            }}
-          />
-          <View
-            style={{
-              justifyContent: 'center',
-              zIndex: 99,
-              position: 'absolute',
-              height: windowHeight,
-              width: windowWidth,
-            }}>
-            <ActivityIndicator size="large" color="#ffffff" />
-            <Text
-              style={{
-                fontWeight: 'bold',
-                textAlign: 'center',
-                fontSize: 18,
-                color: 'white',
-              }}>
-              webview will open in {timer} Seconds
-            </Text>
-          </View>
-        </>
-      )}
+      <>
+        <Image
+          source={{
+            uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+          }}
+          style={{
+            width: windowWidth,
+            height: windowHeight,
+            opacity: 0.6,
+            zIndex: -99,
+          }}
+        />
+        <View
+          style={{
+            justifyContent: 'center',
+            zIndex: 99,
+            position: 'absolute',
+            height: windowHeight,
+            width: windowWidth,
+          }}>
+          {!accessWebView && (
+            <>
+              <View style={{alignItems: 'center'}}>
+                <TextInput
+                  style={{
+                    backgroundColor: 'white',
+                    width: '80%',
+                    marginBottom: 4,
+                  }}
+                  onChangeText={nexText => setUrl(nexText)}
+                  value={url}
+                />
+                <Button
+                  onPress={() => setAccessWebView(true)}
+                  title="Open Web View"
+                  color="#841584"
+                  accessibilityLabel="Learn more about this purple button"
+                />
+              </View>
+            </>
+          )}
+        </View>
+      </>
 
       {accessWebView && (
-        <SafeAreaView style={{height: windowHeight}}>
-          <WebView source={{uri: 'https://www.google.com/'}} />
+        <SafeAreaView
+          style={{
+            height: windowHeight,
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 99,
+            position: 'absolute',
+            width: '100%',
+          }}>
+          <View
+            style={{
+              height: 0.8 * windowHeight,
+              width: 0.8 * windowWidth,
+              borderWidth: 1,
+            }}>
+            <WebView source={{uri: url}} />
+          </View>
         </SafeAreaView>
       )}
 
